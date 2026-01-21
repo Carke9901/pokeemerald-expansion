cc21b85e3cd6e63a2d846e862f7b6593fc8dd9dd
Evan Sanat <evanjsanat@gmail.com>
2026-01-20 22:57:21 -0500

    worked on gym trainer teams

--- include/constants/opponents.h
+++ include/constants/opponents.h
@@ -858,13 +858,20 @@
 #define TRAINER_LEAF                        852
 #define TRAINER_BRENDAN_PLACEHOLDER         853
 #define TRAINER_MAY_PLACEHOLDER             854
+#define TRAINER_ASH                         855
+#define TRAINER_NORMAN_6                    856
+#define TRAINER_CARSON                      857
+#define TRAINER_MASON                       858
+#define TRAINER_ANNA                        859
+#define TRAINER_HIRAM                       860
+#define TRAINER_FRANK                       861
 
 // NOTE: Because each Trainer uses a flag to determine when they are defeated, there is only space for 9 additional trainers before trainer flag space overflows
 //       More space can be made by shifting flags around in constants/flags.h or changing how trainer flags are handled
 //       MAX_TRAINERS_COUNT can be increased but will take up additional saveblock space
 
-#define TRAINERS_COUNT                      855
-#define MAX_TRAINERS_COUNT                  864
+#define TRAINERS_COUNT                      910
+#define MAX_TRAINERS_COUNT                  1000
 #define TRAINER_PARTNER(partner)           (MAX_TRAINERS_COUNT + partner)
 
 #endif  // GUARD_CONSTANTS_OPPONENTS_H
